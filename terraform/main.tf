abe8e41f1347a95264081c0c59216c19ee5ccb63
mariuszbyczek <91824400+mariuszbyczek@users.noreply.github.com>
2022-12-10 16:29:51 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,69 +1,61 @@
-resource "aws_s3_bucket" "my-bucket" {
-  bucket = var.bucketName
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+  tags   = var.tags
 }
-
-resource "aws_s3_bucket_acl" "my-bucket" {
-  bucket = aws_s3_bucket.my-bucket.id
-  acl    = var.aclType
-}
-
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.my-bucket.id
-
-  policy = <<POLICY
-{
-"Version": "2012-10-17",
-"Statement": [
-{
-"Sid": "PublicReadGetObject",
-"Effect": "Allow",
-"Principal": "*",
-"Action": "s3:GetObject",
-"Resource": "arn:aws:s3:::my-bucket/*"
-}
-]
-}
-POLICY
-}
-
-resource "aws_s3_object" "index" {
-  source = var.index_source
-  content_type = var.type_html
-  etag = filemd5(var.index_source)
-  acl = var.acl_public
-  bucket = aws_s3_bucket.my-bucket.bucket
-  key    = var.index_file
-}
-
-resource "aws_s3_object" "error" {
-  bucket = aws_s3_bucket.my-bucket.bucket
-  key    = var.error_file
-  source = var.error_source
-  content_type = var.type_html
-  etag = filemd5(var.error_source)
-  acl = var.acl_public
-}
-
-
-resource "aws_s3_bucket_website_configuration" "my-bucket" {
-  bucket = aws_s3_bucket.my-bucket.bucket
+resource "aws_s3_bucket_website_configuration" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
 
   index_document {
-    suffix = var.index_file
+    suffix = "index.html"
   }
 
   error_document {
-    key = var.error_file
+    key = "error.html"
   }
 
-  routing_rules = <<EOF
-[{
-    "Condition": {
-        "KeyPrefixEquals": "docs/"
-    },
-    "Redirect": {
-        "ReplaceKeyPrefixWith": ""
-    }
-}]
-EOF
+}
+resource "aws_s3_bucket_acl" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          aws_s3_bucket.s3_bucket.arn,
+          "${aws_s3_bucket.s3_bucket.arn}/*",
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_s3_object" "object_www" {
+  depends_on   = [aws_s3_bucket.s3_bucket]
+  for_each     = fileset("${path.root}", "www/*.html")
+  bucket       = var.bucket_name
+  key          = basename(each.value)
+  source       = each.value
+  etag         = filemd5("${each.value}")
+  content_type = "text/html"
+  acl          = "public-read"
+}
+
+resource "aws_s3_object" "object_assets" {
+  depends_on = [aws_s3_bucket.s3_bucket]
+  for_each   = fileset(path.module, "assets/*")
+  bucket     = var.bucket_name
+  key        = each.value
+  source     = "${each.value}"
+  etag       = filemd5("${each.value}")
+  acl        = "public-read"
 }
